dd3d4a4d08ed65758185e6b1bf3447bac7d0d303
s.pierre <s.pierre@criteo.com>
2024-11-28 10:23:03 +0100

    OhceTest done

--- java/src/test/java/info/dmerej/MockConsoleInteractor.java
+++ java/src/test/java/info/dmerej/MockConsoleInteractor.java
@@ -3,17 +3,32 @@ package info.dmerej;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MockConsoleInteractor extends ConsoleInteractor{
-    private List<String> messages;
+public class MockConsoleInteractor extends ConsoleInteractor {
+    private final List<String> inputs;
+    private int currentInputIndex;
+    private final List<String> messages;
 
     public MockConsoleInteractor() {
+        this.inputs = new ArrayList<>();
+        this.currentInputIndex = 0;
         this.messages = new ArrayList<>();
     }
 
-    public String readInput(String input) {
-        return input;
+    public void setInputs(List<String> inputs) {
+        this.inputs.clear();
+        this.inputs.addAll(inputs);
+        this.currentInputIndex = 0;
     }
 
+    @Override
+    public String readInput() {
+        if (currentInputIndex < inputs.size()) {
+            return inputs.get(currentInputIndex++);
+        }
+        return "";
+    }
+
+    @Override
     public void printMessage(String message) {
         System.out.println(message);
         this.messages.add(message);
@@ -22,5 +37,4 @@ public class MockConsoleInteractor extends ConsoleInteractor{
     public List<String> getMessages() {
         return this.messages;
     }
-
 }
--- java/src/test/java/info/dmerej/OhceTest.java
+++ java/src/test/java/info/dmerej/OhceTest.java
@@ -5,37 +5,22 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class OhceTest {
   @Test
   void testMainLoop() {
-    /*
-     TODO: check that given the following inputs:
-      - hello
-      - oto
-     - quit
-
-    The following messages are shown to the user:
-     - olleh
-     - oto
-     - That was a palindrome!
-
-    */
-    //Arrange
+    // Arrange
     MockConsoleInteractor consoleInteractor = new MockConsoleInteractor();
+    consoleInteractor.setInputs(List.of("hello", "oto", "quit"));
     Ohce ohce = new Ohce(consoleInteractor);
 
-    //Act
-    consoleInteractor.readInput("hello");
-    consoleInteractor.readInput("oto");
-    consoleInteractor.readInput("quit");
+    // Act
     ohce.mainLoop();
 
-    //Assert
+    // Assert
     List<String> res = consoleInteractor.getMessages();
-    assertEquals(res.get(0), "olleh");
-    assertEquals(res.get(1), "oto");
-    assertEquals(res.get(2), "That was a palindrome!");
+    assertEquals("olleh", res.get(0));
+    assertEquals("oto", res.get(1));
+    assertEquals("That was a palindrome!", res.get(2));
   }
 }
